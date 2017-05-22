5c567168e2927644a377d78443d559655eb4aabf
codywohlers <code@codywohlers.ca>
2017-05-21 20:37:32 -0600

    initial creation

---
+++ README.md
@@ -0,0 +1,7 @@
+Some Unreal Editor Linux helper files
+2017-May-21 code@codywohlers.ca
+
+update-unreal - a script to download and compile the latest Unreal Editor for Linux.
+UnrealEditor.desktop - a .desktop file 
+
+(assuming you installed Unreal Editor in /opt/UnrealEditor)
---
+++ update-unreal.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# udpate-unreal
+# downloads and compiles Unreal Engine from Epic's github account.
+# based on https://wiki.unrealengine.com/Building_On_Linux
+# (see also https://github.com/EpicGames/UnrealEngine/tree/release/Engine/Build/BatchFiles/Linux)
+
+# 2015-May-21 code@codywohlers.ca - initial creation.
+
+
+UNREAL_DIR="/opt/UnrealEngine"  # must be owned by you (don't use sudo)
+
+set -e
+
+cd "$UNREAL_DIR"
+
+git pull https://github.com/EpicGames/UnrealEngine.git release  # must have linked your github account to your epic account.
+
+./Setup.sh
+./GenerateProjectFiles.sh
+
+if [ "$1" == "-clean" ] ;then
+make CrashReportClient-Linux-Shipping \
+	ShaderCompileWorker \
+	UnrealPak \
+	UnrealLightmass \
+	UnrealFrontend \
+	UE4Editor \
+	ARGS=-clean
+fi
+make
+
