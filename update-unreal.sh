2130f82e39cdb6c8bb3ddd1fc3d0ea1226041c80
codywohlers <code@codywohlers.ca>
2017-08-24 02:24:57 -0600

    added -partial-clean argument.

--- update-unreal.sh
+++ update-unreal.sh
@@ -6,6 +6,7 @@
 # based on https://wiki.unrealengine.com/Building_On_Linux
 # (see also https://github.com/EpicGames/UnrealEngine/tree/release/Engine/Build/BatchFiles/Linux)
 
+# 2017-Aug-24 code@codywohlers.ca - added "-partial-clean" argument.
 # 2017-Jul-03 code@codywohlers.ca - added UnrealPak to make target.
 # 2017-Jun-11 code@codywohlers.ca - added check option.
 # 2017-May-24 code@codywohlers.ca - updated usage description.
@@ -38,5 +39,13 @@ make CrashReportClient-Linux-Shipping \
 	UE4Editor \
 	ARGS=-clean
 fi
+
+if [ "$1" == "-partial-clean" ] ;then
+    find Engine/Intermediate/Build/Linux/ -name PCH.Core.h.gch -exec rm -v '{}' \;
+    find Engine/Intermediate/Build/Linux/ -name SharedPCH.Core.h.gch -exec rm -v '{}' \;
+    find Engine/Intermediate/Build/Linux/ -name SharedPCH.CoreUObject.h.gch -exec rm -v '{}' \;
+    find Engine/Intermediate/Build/Linux/ -name PCH.CoreUObject.h.gch -exec rm -v '{}' \;
+fi
+
 make
 make UnrealPak
